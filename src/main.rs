b2799c5f23404813951b39bf27b3f2b7d93d402d
Mac Chaffee <me@macchaffee.com>
2024-04-22 19:42:17 -0400

    Test out implementation with sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use poise::serenity_prelude as serenity;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+// User data, which is stored and accessible in all command invocations
+struct Data {
+    database: sqlx::SqlitePool,
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
@@ -11,6 +14,15 @@ async fn age(
     #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let id = u.id.get() as i64;
+    sqlx::query!(
+            "INSERT INTO todo (task, user_id) VALUES (?, ?)",
+            "test",
+            id,
+        )
+        .execute(&ctx.data().database) // < Where the command will be executed
+        .await
+        .unwrap();
     let response = format!("{}'s account was created at {}", u.name, u.created_at());
     ctx.say(response).await?;
     Ok(())
@@ -19,6 +31,19 @@ async fn age(
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    // Initiate a connection to the database file, creating the file if required.
+    let database = sqlx::sqlite::SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect_with(
+            sqlx::sqlite::SqliteConnectOptions::new()
+                .filename("database.sqlite")
+                .create_if_missing(true),
+        )
+        .await
+        .expect("Couldn't connect to database");
+
+    sqlx::migrate!("./migrations").run(&database).await.expect("Couldn't run database migrations");
+
     // No intents since we only support slash commands
     let intents = serenity::GatewayIntents::empty();
 
@@ -30,7 +55,7 @@ async fn main() {
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {database})
             })
         })
         .build();
@@ -38,5 +63,6 @@ async fn main() {
     let client = serenity::ClientBuilder::new(token, intents)
         .framework(framework)
         .await;
+    // client.unwrap().http.send_message(channel_id, files, map);
     client.unwrap().start().await.unwrap();
 }
